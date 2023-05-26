6ec366e338e842a416d1e2aeb75fc89c97e231b2
Pierre Verdiere <pverdiere59@gmail.com>
2023-05-26 09:10:52 +0200

    feat: modified helloWorld function by multiply function

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,16 @@
 #! /usr/bin/env node
-console.log("Hello, world");
+const args = process.argv.slice(2);
+if (args.length < 2){
+    console.error("Please enterat least 2 numbers");
+    process.exit(1);
+}
+
+const total = args.reduce((previous, current) => parseFloat(current) * parseFloat(previous))
+
+if (isNaN(total)) {
+    console.error("One or more arguments are not numbers");
+    process.exit(1);
+}
+
+console.log(total);
+process.exit(0);
